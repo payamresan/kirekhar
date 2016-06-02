45d3a1d355b6fe031c8102d89afa75a1d661a57e
payamresan <sourceblackplus5@gmail.com>
2016-06-02 16:14:36 -0700

    Update poker.lua

--- plugins/poker.lua
+++ plugins/poker.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = '😐'
+local text = '😐پـــوکـــر نـــفـــرســـ'
 if matches[1] == '😐' then
     if not is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
